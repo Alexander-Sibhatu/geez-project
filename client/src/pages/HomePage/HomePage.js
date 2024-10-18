1cb4790d38e12d79cc4820b561a314ecea6e6a45
bisruxa <imbisru@gmail.com>
2024-10-18 02:00:01 -0900

    home page

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,17 +1,40 @@
-import React from 'react'
-import './homepage.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import classes from "./homepage.module.css";
+import { Link } from "react-router-dom";
+import angle_icon from '../../assets/angle_icon.svg'
+import search_icon from '../../assets/search_icon.svg'
 
 const HomePage = () => {
   return (
+    <div className={classes.HomePage}>
+      <div className={classes.Icon}>
+        <img src={angle_icon} alt="" className={classes.leftIcon} />
 
-    <div className='HomePage'>
-      <button><Link to="/catagory">Catagory</Link></button>
-      <button><Link to="/grammer">Grammer</Link></button>
-      <button><Link to ="/noun">Noun</Link></button>
-      {/* <button><Link to ="/mistakes">Mistakes</Link></button> */}
-      <button><Link to="/poem">Poem</Link></button>
+        <img src={angle_icon} alt="" className={classes.rightIcon} />
+      </div>
+      <div className={classes.SearchBar}>
+        <input type="text" placeholder="ፈልግ" className={classes.searchBar}/>
+        <img src={search_icon} alt="" className={classes.searchIcon}/>
+      </div>
+      <div className={classes.buttons}>
+        <button>
+          <Link to="">ግሥ</Link>
+        </button>
+
+        <button>
+          <Link to="">ነባር</Link>
+        </button>
+        <button>
+          <Link to="">የቅኔ መጠነ ዜማ</Link>
+        </button>
+        <button>
+          <Link to="">አገባብ</Link>
+        </button>
+        <button>
+          <Link to="">ጸያፍ</Link>
+        </button>
+      </div>
     </div>
-  )
-}
-export default HomePage
+  );
+};
+export default HomePage;
