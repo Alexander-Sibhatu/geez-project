ce1fa7f502a3d8ee271ba5a8aef745526ed0c849
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-10-22 11:28:14 +0100

    dummy data for fetch added.

---
+++ client/src/features/useFetch.js
@@ -0,0 +1,53 @@
+// Let's define the useFetch function here
+// Dummy data is given below. Pretend that this is the data that will be fetched from the url
+
+const dummyData = [
+    {
+        "Gis": "ሰላም",
+        "phonetic": "selam",
+        "part_of_speech": "noun",
+        "translation": "peace",
+        "root": "ሰላም",
+        "plural": "ሰላማት",
+        "synonyms": "ፍቅር",
+        "antonyms": "ጸብእ",
+        "example_usage": "ሰላም በሰላም ይነብር።",
+        "derived_forms": "ሰላማዊ",
+        "etymology": "Geez origin, meaning tranquility",
+        "gender": "neutral",
+        "audio_pronunciation": null,
+        "notes": "Used as a greeting and concept of peace"
+      },
+      {
+        "Gis": "ምንዳቤ",
+        "phonetic": "mindabe",
+        "part_of_speech": "noun",
+        "translation": "idea",
+        "root": "ምንዳቤ",
+        "plural": "ምንዳቤያት",
+        "synonyms": "ዓጸባ",
+        "antonyms": "ጣዕም",
+        "example_usage": "ረከቦሙ ምንዳቤ ለዮሴፍ ወማርያም።",
+        "derived_forms": null,
+        "etymology": "Innovative thinking in Geez culture",
+        "gender": "neutral",
+        "audio_pronunciation": null,
+        "notes": "Represents thoughts or concepts"
+      },
+      {
+        "Gis": "ስም",
+        "phonetic": "sim",
+        "part_of_speech": "noun",
+        "translation": "name",
+        "root": "ስም",
+        "plural": "አስማት",
+        "synonyms": "ተጸውዖ",
+        "antonyms": null,
+        "example_usage": "ስም ይመርሕ ወማሕቶት ያበርህ።",
+        "derived_forms": "ተሰምየ",
+        "etymology": "Literal meaning of identification",
+        "gender": "neutral",
+        "audio_pronunciation": null,
+        "notes": "Found in spiritual texts"
+      },
+]
