5fdfacda623bb5af83d80837462697eb6b4d7c4c
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-10-28 10:24:27 +0000

    fetched verb display test done

--- client/src/pages/VerbPage/VerbPage.js
+++ client/src/pages/VerbPage/VerbPage.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchVberbs } from '../../features/useFetch'
+import classes from './verbPage.module.css'
+
 
 const VerbPage = () => {
+  const [verbs, setVerbs] = useState([])
+
+  const fetchData = async () => {
+    const data = await fetchVberbs();
+    setVerbs(data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  })
+
+  const verb = verbs.map((verb, index) => (
+    <div key={index} className={classes.verb}>
+      <p><strong>Gis: </strong> {verb.Gis}</p>
+    </div>
+  ))
+
   return (
-    <div>VerbPage</div>
+    <div>{verb}</div>
   )
 }
 
